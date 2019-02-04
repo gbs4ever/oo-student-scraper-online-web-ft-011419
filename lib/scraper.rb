a3d348773f177fe90fa26595cd4e084a108f20d8
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-04 00:25:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,6 +21,7 @@ data
       #this is the links css   (".social-icon-container a") .attribute("href").value
       links=doc.css(".social-icon-container a").map{|data|data.attribute("href").value}
       links.each do|tag|
+        binding.pry
                 if links.include?(tag)
                   file[:linkedin]="tag"
                   elsif links.include?(tag)
